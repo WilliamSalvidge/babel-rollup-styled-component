b1f77ab160c2cba0953a7ad91f2ada92e8dd6a87
William Salvidge <willsalvidge@googlemail.com>
2022-06-10 10:30:24 +0100

    added request to db server

--- src/app.js
+++ src/app.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import StyledTestButton from "./button.js";
 import StyleButton from "./styledButton.js";
 import StyledDiv from "./styledDiv.js";
 
 const App = () => {
-    return (
-        <>
-            <h1>Hey again!!!</h1>
-            <StyledDiv>
-                <StyledTestButton onClick={() => alert('Hey Will!')}/>
-                <StyleButton onClick={() => alert('Hey Will!')}>Primary Button</StyleButton>
-                <StyleButton primary>Secondary Button</StyleButton>
-            </StyledDiv>
-        </>
-    )
-}
+  const [isPush, setIsPush] = useState();
 
-const container = document.getElementById('root');
+  const sendRequest = async () => {
+    const result = await fetch(
+      "http://localhost:8081"
+    );
+    const text = await result.text();
+    setIsPush(text);
+  };
+
+  return (
+    <>
+      <h1>Hey again!!!</h1>
+      <StyledDiv>
+        <StyledTestButton
+          onClick={() => sendRequest()}
+        />
+        <StyleButton
+          onClick={() => alert("Hey Will!")}
+        >
+          Primary Button
+        </StyleButton>
+        <StyleButton primary>
+          Secondary Button
+        </StyleButton>
+      </StyledDiv>
+      {isPush ? <p>{isPush}</p> : null}
+    </>
+  );
+};
+
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />)
+root.render(<App />);
